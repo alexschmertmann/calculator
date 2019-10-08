38a30b325df4cda4cc83b383065078b2cce800bc
Alex Schmertmann <alex-schmertmann@uiowa.edu>
2019-10-08 18:43:14 -0500

    Jinja additions

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html', pageTitle = 'MyCalculator')
 
 @app.route('/Alex')
 def Alex():
-    return "Hello Alex"
+    return render_template('Alex.html', pageTitle = 'Alex')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
