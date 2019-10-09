7e59c6ca64b27ba4271c31ec3d7b174e1e854992
Alex Schmertmann <alex-schmertmann@uiowa.edu>
2019-10-08 19:28:47 -0500

    simple addition calculator

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle = 'MyCalculator')
+    return render_template('index.html', display ="", pageTitle = 'MyCalculator')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        form = request.form
+        numberone = int(form['numOne'])
+        numbertwo = int(form['numTwo'])
+        calc = numberone + numbertwo
+        return render_template('index.html', display= calc, pageTitle='MyCalculator')
+
+    return redirect("/")
+
 
 @app.route('/Alex')
 def Alex():
