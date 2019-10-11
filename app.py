47da920f352f9a53a894ac9e6b815db7901611aa
Alex Schmertmann <alex-schmertmann@uiowa.edu>
2019-10-11 00:07:16 -0500

    Working Loan Calculator

--- app.py
+++ app.py
@@ -7,21 +7,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', display ="", pageTitle = 'MyCalculator')
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/Calculate', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         form = request.form
-        numberone = int(form['numOne'])
-        numbertwo = int(form['numTwo'])
-        calc = numberone + numbertwo
-        return render_template('index.html', display= calc, pageTitle='MyCalculator')
+        A = float(form['LoanAmt'])
+        n = float(form['NumYears'])*12
+        i = float(form['Interest']) / 12
+        D = ((( 1 + i ) **n ) - 1 ) / ( i * ( 1+ i) **n)
+        LoanPmt = A / D
+        return render_template('index.html', display= LoanPmt, pageTitle='MyCalculator')
 
     return redirect("/")
 
-
-@app.route('/Alex')
-def Alex():
-    return render_template('Alex.html', pageTitle = 'Alex')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
